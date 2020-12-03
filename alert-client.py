19cec66ec0e19085564af54db2aa367a5541b09b
mike <mgmike1023@aol.com>
2020-12-03 16:41:12 -0500

    Added keep trying to connect

--- alert-client.py
+++ alert-client.py
@@ -2,19 +2,22 @@
 import RPi.GPIO as GPIO
 import time
 import socket
+import struct
 
 HOST = '192.168.50.220'
 #HOST = '127.0.0.1'
 PORT = 65432
+PIN_TRIGGER = 7
+PIN_ECHO = 11
 
 
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((HOST, PORT))
+        while error is OSError:
+                error = s.connect((HOST, PORT))
+                time.sleep(2)
 
 	GPIO.setmode(GPIO.BOARD)
-	PIN_TRIGGER = 7
-	PIN_ECHO = 11
 
 	GPIO.setup(PIN_TRIGGER, GPIO.OUT)
 	GPIO.setup(PIN_ECHO, GPIO.IN)
@@ -28,11 +31,8 @@ try:
 	print("Calculating distance")
 
 	while True:
-
 		GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-
 		time.sleep(0.00001)
-
 		GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
 		while GPIO.input(PIN_ECHO)==0:
@@ -41,18 +41,16 @@ try:
 			pulse_end_time = time.time()
 
 		pulse_duration = pulse_end_time - pulse_start_time
-
 		distance = round(pulse_duration * 17150, 2)
-		dist_str = "Signal: " + str(distance) 
-		dist_bytes = dist_str.encode('utf-8')
-		print(dist_str)
+		dist_bytes = str(distance).encode('utf-8')
+		print(str(distance))
 
-		time.sleep(0.3)
+		time.sleep(0.5)
 
-		if distance < 130:
-			s.sendall(dist_bytes)
-			data = s.recv(1024)
-			print('Received', repr(data))
+		#byte_data = get_dist()
+		s.sendall(dist_bytes)
+		data = s.recv(1024)
+		print('Received', repr(data))
 
 finally:
 	GPIO.cleanup()
