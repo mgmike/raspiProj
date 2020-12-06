399366c82eb893342f6e135ccab38b5a2e9ff8f3
Mike <mgmike1023@aol.com>
2020-12-06 14:38:08 -0500

    finds ip given driver

--- alert-server.py
+++ alert-server.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 import socket
+import netifaces as ni
 
 HOST = '192.168.50.220'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def get_ip(hostname):
+	ip = ni.ifaddresses(hostname)[ni.AF_INET][0]['addr']
+	return ip
+
+def get_ip_raspi():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	hostname = socket.getfqdn()
+	(HOST, alias, iplist) = socket.gethostbyname_ex(hostname)
+	alias_str = ''.join([i for i in iplist])
+	print(hostname + " ip, fqdn: " + HOST + ', ' + alias_str)
+
+HOST = get_ip('wifi0')
+print(HOST)
+
+#except:
+#	HOST = socket.gethostbyname(socket.gethostname())
+#	print("Host ip: " + HOST)
+
 s.bind((HOST, PORT))
 while True:
 	s.listen()
