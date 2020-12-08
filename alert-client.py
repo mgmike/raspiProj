8bdbc765e12d4d5cffc5645044887df8e7b5c7d8
Mike <mgmike1023@aol.com>
2020-12-08 13:05:14 -0500

    Swapped server and client, client side

--- alert-client.py
+++ alert-client.py
@@ -1,87 +1,52 @@
 #!/usr/bin/python3
-import RPi.GPIO as GPIO
 import time
 import socket
 import struct
+import queue
+from threading import Thread
 
-HOST = '192.168.50.220'
-#HOST = '192.168.50.143'
+#HOST = '192.168.50.220'	#Desktop
+#HOST = '192.168.50.143'	#
 #HOST = '127.0.0.1'
+HOST = '192.168.50.183'		#RasPiZero1
 PORT = 65432
-PIN_TRIGGER = 7
-PIN_ECHO = 11
 
 threshold = 120
 delta = 20
-certanty = 0.0
 
-def send_ack(dist_bytes, s):
-	s.sendall(dist_bytes)
-	data = s.recv(1024)
-	print('Received', repr(data))
+q = queue.Queue(100)
 
-
-def process_data(data, certanty):
-	data_s = data.decode("utf-8")
-	data_d = Decimal(data_s)
-	if data_d < threshold:
-		if certanty < 1.0:
-			certanty += 1 / delta
-	else:
-		if certanty > 0.0:
-			certanty -= 1 / delta
-	if certanty > 0.5:
-		print("Danger!")
-	return certanty
-
-def read_sensor(s):
-
-	GPIO.setmode(GPIO.BOARD)
-
-	GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-	GPIO.setup(PIN_ECHO, GPIO.IN)
-
-	GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-	print("Waiting for sensor to settle")
-
-	time.sleep(2)
-
-	print("Calculating distance")
-
-	pulse_start_time = 0
-	pulse_end_time = 0
-
+def get_data(s, q):
 	while True:
-		GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-		time.sleep(0.00001)
-		GPIO.output(PIN_TRIGGER, GPIO.LOW)
+		data_s = s.recv(1024).decode("utf-8")
+		while data_s is None or not data_s:
+			data_s = s.recv(1024).decode("utf-8")
+		for reading in data_s.split(','):
+			if reading:
+				q.put(float(reading))
 
-		while GPIO.input(PIN_ECHO)==0:
-			pulse_start_time = time.time()
-		while GPIO.input(PIN_ECHO)==1:
-			pulse_end_time = time.time()
 
-		pulse_duration = pulse_end_time - pulse_start_time
-		distance = round(pulse_duration * 17150, 2)
-		dist_bytes = str(distance).encode('utf-8')
-		print(str(distance))
+def process_data(q):
+	certanty = 0.0
+	while True:
+		if q.get() < threshold:
+			if certanty < 1.0:
+				certanty += 1 / delta
+		else:
+			if certanty > 0.0:
+				certanty -= 1 / delta
+		if certanty > 0.5:
+			print("Danger!")
 
-		time.sleep(0.1)
 
-		send_ack(dist_bytes, s)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+error = s.connect((HOST, PORT))
+thread_get = Thread(target = get_data, args = (s,q))
+thread_process = Thread(target = process_data, args = [q])
 
-try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while error is OSError:
 	error = s.connect((HOST, PORT))
+	time.sleep(1)
 
-	while error is OSError:
-		error = s.connect((HOST, PORT))
-		time.sleep(1)
-
-	read_sensor(s)
-	
-
-finally:
-	GPIO.cleanup()
-	print("done!")
+thread_get.start()
+thread_process.start()
